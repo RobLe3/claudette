5ee5d538ef5aa0eea8bbfd676106cf29a7af40f8
Roble Mumin <github@roblemumin.com>
2025-08-18 14:28:20 +0200

    🧹 Clean up test artifacts and consolidate utilities

--- src/test/utils/index.js
+++ src/test/utils/index.js
@@ -1,35 +1,9 @@
-#!/usr/bin/env node
-
-/**
- * Test Utilities Index
- * 
- * Consolidated export of all test infrastructure utilities.
- * Provides a single entry point for accessing test consolidation tools.
- */
-
-const TestRunner = require('./TestRunner');
-const TestBase = require('./TestBase');
-const ErrorHandler = require('./ErrorHandler');
-const DefaultConfiguration = require('./DefaultConfiguration');
+// Consolidated test utilities
+const { TestRunner, runTestSuite } = require('./test-runner');
+const { TestBase } = require('./test-base');
 
 module.exports = {
   TestRunner,
   TestBase,
-  ErrorHandler,
-  DefaultConfiguration,
-  
-  // Convenience factory methods
-  createTestRunner: (options) => new TestRunner(options),
-  createTestBase: (options) => new TestBase(options),
-  createErrorHandler: (options) => new ErrorHandler(options),
-  createConfiguration: (options) => new DefaultConfiguration(options),
-  
-  // Static helper methods
-  runTest: TestRunner.run,
-  handleError: ErrorHandler.handle,
-  retryOperation: ErrorHandler.retry,
-  categorizeError: ErrorHandler.categorize,
-  getTestConfig: DefaultConfiguration.forTestType,
-  getBackendConfig: DefaultConfiguration.forBackend,
-  getRAGConfig: DefaultConfiguration.forRAG
+  runTestSuite
 };
